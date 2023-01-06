02db6e749d4264ea18ad60b12ba2b3372e5820be
dluca22 <luc.due@tim.it>
2023-01-06 15:09:45 +0100

    added keys to question components

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import { decodeHtml } from './helper_functions'
+import Question from './Question'
+import Data from "./fakeAPI"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [game, setGame] = useState([])
+  const [ended, setEnded] = useState(false)
 
-  return (
-    <div className="App">
-      <p>prova, ciao</p>
+// on game start or gameEnded sends a request to API
+// on API response, adds 2 keys to each question Obj : "guessed" and "selected" 
+  useEffect(() =>{
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+    .then(res => res.json())
+    .then(data => {
+        let gameQuestions = data.results.map(quest => ({...quest, guessed: false, selected:false}))
+        console.log(gameQuestions)
+        return setGame(data.results)})
+}, [])
+
+
+    const items = game.map((item, index) => {
+        let answers = [...item.incorrect_answers, item.correct_answer]
+
+        return  <Question key={index} question={decodeHtml(item.question)} choices={answers} />
+        }
+     )
+
+     let button;
+     if(game === false){
+        button = <button className="submit" onClick={console.log("submit")}>Submit</button>
+
+    }else{
+       button = <button className="new-game" onClick={() => setGame(true)}>Play Again?</button>
+     }
+
+
+     return (
+         <div className="App">
+        {items}
+        {button}
     </div>
   )
 }
