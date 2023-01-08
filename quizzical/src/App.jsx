5a0cfc9566533aefd7b7e5536074312264159e20
dluca22 <luc.due@tim.it>
2023-01-08 17:28:00 +0100

    added logic for correct/wrong className on gameEnd

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -9,12 +9,13 @@ function App() {
   const [round, setRound ] = useState(0)
     // state to hold array of quiz questions
   const [quiz, setQuiz] = useState([]);
-  // on submit button set ended = true, check answers and if 5/5 play confetti (maybe set separate state)
-  const [ended, setEnded] = useState(false);
+  // on submit button set gameEnded = true, check answers and if 5/5 play confetti (maybe set separate state)
+  const [gameEnded, setGameEnded] = useState(false);
   // new state for tracking selected answers, object with {id(key):userChoice(value)}
   const [selected, setSelected] = useState({})
   // keep track of result
   const [result, setResult]= useState(0)
+
 
   // on round update sends a request to API
   useEffect(() => {
@@ -46,46 +47,79 @@ function App() {
     setSelected(prevState =>({...prevState, [id] :  prevState[id]== userChoice? "" : userChoice }))
   }
 
+  function sortAnswersOnce(original, copy){
+
+    console.log("original : ", original, "copy", copy)
+    // if it is TRUE that the 2 arrays are not equal, execute the shuffling
+    if (JSON.stringify(original) != JSON.stringify(copy)){
+        return copy.sort(() => Math.random() - 0.5)
+    }else{
+        console.log("else-copy", copy)
+        return copy
+    }
+
+
+  }
 
   // generate <Question /> components, passing question, answer-options, the `selected` value for this question component and the function to handle setSelected
   const questions = quiz.map((item, index) => {
 
-    // shuffle answer options before passing prop
-    let answers = [...item.incorrect_answers, item.correct_answer].sort(() => Math.random() - 0.5);
+    // create copy of answers array to use for shuffling
+    // then pass the props after checking if it is already shuffled
+    let allAnswers = [...item.incorrect_answers, item.correct_answer]
+
+    let answers = [... allAnswers].sort(() => Math.random() - 0.5)
+    console.log("1 allAnswers : ", allAnswers, "answers-copy : ", answers)
 
     return (
         <Question
         key={index}
         id={index}
         question={item.question}
-        choices={answers}
+        choices={sortAnswersOnce(allAnswers, answers)}
         selected={selected[index]}
+        correct_answer={quiz[index].correct_answer}
+        gameEnded={gameEnded}
         handleChoice={selectAnswer}/>
         );
     });
 
-    function submitMatch(){
+    useEffect(()=>{
+        if(gameEnded == true){
+            console.log("qui")
+            function submitAnswers(){
+            // map over quiz array, for each question check if correct_answer == selected[id] value
+            quiz.map((question, index) => {
+                setResult(()=> {
+                    question.correct_answer == selected[index] ? result + 1 : result
+                })
+            })}
+        }
+        /* else { MA NON SO se ha senso in quanto non so se vengono azionati ogni volta che viene cliccato il tasto e la condizione è `false`, può essere che non vengano mai azionati
+            setSelected({})
+            setResult(0)
 
-    }
+        } */
+    }, [gameEnded])
 
   return (
       <div className="App">
-      {ended && result == 5 && <Confetti />}
+      {gameEnded && result == 5 && <Confetti />}
 
     <div className="round-counter"> Round: {round}</div>
 
     {/* if round == 0 ? welcome screen + btn : game page at round 1+ */}
 
       {questions}
-      {ended ? (<button className="new-game"
+      {gameEnded ? (<button className="new-game"
                 onClick={() => setRound(round => round +1)}
                 >Play Again?</button>)
 
             :  (<button className="submit"
-                onClick={()=> submitMatch()}
+                onClick={()=> setGameEnded(true)}
                 >Submit</button>) }
 
-      {ended && <div> correct = {result}/5</div>}
+      {gameEnded && <div> correct = {result}/5</div>}
     </div>
   );
 }
--- quizzical/src/components/Question.jsx
+++ quizzical/src/components/Question.jsx
@@ -8,9 +8,21 @@ export default function Question(props) {
     // map over array of choices to create button
     let choices = props.choices.map((choice) => {
         // adds selected class to the option saved in `selected` state
-        let dynamic_class =
-        props.selected != choice
+        let dynamic_class
+
+        // if game not ended add selected class to the choice stored in `selected` state
+        if(props.gameEnded == false){
+        dynamic_class = props.selected != choice
             ? "option" : "option selected";
+        }
+        // else on game ended, add "correct" class to the choice == quiz.correct_answer
+        else if(props.gameEnded){
+            dynamic_class = choice == props.correct_answer ? "option correct" : "option"
+                // but if this choice is the one in `selected` state AND NOT correct, append "wrong"
+            if ( props.selected == choice && props.correct_answer != choice){
+                dynamic_class += " wrong"
+            }
+        }
 
         return ( <button
                         key={choice}
