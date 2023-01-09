9e5cad8100d8f7f51f15817928cf6a358635664e
dluca22 <luc.due@tim.it>
2023-01-09 09:51:46 +0100

    testing using local file as fake API call

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -1,65 +1,89 @@
 import { useState, useEffect } from "react";
 import { decodeHtml, shuffle } from "./helper_functions";
 import Question from "./components/Question";
-import Confetti from "react-confetti"
-import Navbar from "./components/Navbar"
+import Confetti from "react-confetti";
+import Navbar from "./components/Navbar";
+import fakeAPI from "./fakeAPI";
 
 export default function App() {
-
-    // keep track of round played, starts at 0 in welcome page
-  const [round, setRound ] = useState(0)
+  // keep track of round played, starts at 0 in welcome page
+  const [round, setRound] = useState(0);
   // keep track of result for the round
-  const [result, setResult]= useState(0)
+  const [result, setResult] = useState(0);
   // keep track of global score
-  const[globalScore, setGlobalScore] = useState(0)
+  const [globalScore, setGlobalScore] = useState(0);
 
   // state to hold array of quiz questions
   const [quiz, setQuiz] = useState([]);
   // on submit button set gameEnded = true, check answers and if n/n play confetti
   const [gameEnded, setGameEnded] = useState(false);
   // new state for tracking selected answers, object with {id(key):userChoice(value)}
-  const [selected, setSelected] = useState({})
-
+  const [selected, setSelected] = useState({});
 
   // on round update sends a request to API
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=3&type=multiple")
+    // ==== REAL API CALL ===========================================================
+
+    /*fetch("https://opentdb.com/api.php?amount=3&type=multiple")
       .then((res) => res.json())
       .then((data) => {
-
         // create dynamic object based on num of questions to store user answers
         const userAnswers = data.results.reduce((acc, item, index) => {
-            acc[index] = "";
-            return acc;
+          acc[index] = "";
+          return acc;
         }, {});
-        setSelected(userAnswers)
-
+        setSelected(userAnswers);
 
         // add all answer options in one key, then decode the html content and shuffle order
         let quizData = data.results.map((quest) => ({
-            question: `${decodeHtml(quest.question)}`,
-            all_answers: shuffle(
-                            decodeHtml(
-                                    [...quest.incorrect_answers, quest.correct_answer]
-                                    )),
-            correct_answer: decodeHtml(quest.correct_answer),
-
+          question: `${decodeHtml(quest.question)}`,
+          all_answers: shuffle(
+            decodeHtml([...quest.incorrect_answers, quest.correct_answer])
+          ),
+          correct_answer: decodeHtml(quest.correct_answer),
         }));
         return setQuiz(quizData);
-      });
+      });*/
+    // ==== END REAL API CALL ===========================================================
+
+    // ======= TEMP FAKE API CALL ============================================================
+    // getting data from a local file copy from openTDB,
+    // same as the API, reduce() creates an object for storing userAnswers
+    // then map creates array of question object to set in quiz State
+
+    // setting obj for user Answers
+    const userAnswers = fakeAPI.results.reduce((acc, item, index) => {
+      acc[index] = "";
+      return acc;
+    }, {});
+    setSelected(userAnswers);
+
+    // setting data to quiz State
+    let testData = fakeAPI.results.map((quest) => ({
+      question: `${decodeHtml(quest.question)}`,
+      all_answers: shuffle(
+        decodeHtml([...quest.incorrect_answers, quest.correct_answer])
+      ),
+      correct_answer: decodeHtml(quest.correct_answer),
+    }));
+    setQuiz(testData);
+
+    // ======= END TEMP FAKE API CALL ============================================================
   }, [round]);
 
   // updates the `selected` state object to store userChoice for the corresponding question id
   function selectAnswer(id, userChoice) {
-        // ternary operator allows to chose between values, or de-select it to revert to default empty value
-    setSelected(prevState =>({...prevState, [id] :  prevState[id]== userChoice? "" : userChoice }))
+    // ternary operator allows to chose between values, or de-select it to revert to default empty value
+    setSelected((prevState) => ({
+      ...prevState,
+      [id]: prevState[id] == userChoice ? "" : userChoice,
+    }));
   }
-
 
   // generate <Question /> components, passing question, answer-options, the `selected` value for this question component and the function to handle setSelected
   const questions = quiz.map((item, index) => {
     return (
-        <Question
+      <Question
         key={index}
         id={index}
         question={item.question}
@@ -67,51 +91,52 @@ export default function App() {
         selected={selected[index]}
         correct_answer={quiz[index].correct_answer}
         gameEnded={gameEnded}
-        handleChoice={selectAnswer}/>
-        );
-    });
+        handleChoice={selectAnswer}
+      />
+    );
+  });
 
-
-
-    useEffect(()=>{
-        if(gameEnded == true){
-            let score = 0
-            // map over quiz array, for each question check if correct_answer == selected[id] value
-            quiz.map((question, index) => {
-                score = question.correct_answer == selected[index] ? score + 1 : score
-
-            })
-        setResult(score)
-        }
-    }, [gameEnded])
-
-
-
-    function newGame(){
-        setRound(round => round +1)
-        setGlobalScore(prevScore => prevScore + result)
-        setGameEnded(false)
+  useEffect(() => {
+    if (gameEnded == true) {
+      let score = 0;
+      // map over quiz array, for each question check if correct_answer == selected[id] value
+      quiz.map((question, index) => {
+        score = question.correct_answer == selected[index] ? score + 1 : score;
+      });
+      setResult(score);
     }
+  }, [gameEnded]);
 
-    // TODO add and edit Navbar
-    // <Navbar />
+  function newGame() {
+    setRound((round) => round + 1);
+    setGlobalScore((prevScore) => prevScore + result);
+    setGameEnded(false);
+  }
+
+  // TODO add and edit Navbar
+  // <Navbar />
   return (
-      <div className="App">
-      {gameEnded && result == 3 && <Confetti />}
+    <div className="App">
+      {gameEnded && result == 5 && <Confetti />}
 
-    <div className="round-counter"> Round: {round}</div>
-    <div className="round-counter"> Global Score: {globalScore} / {round * 3}</div>
+      <div className="round-counter"> Round: {round}</div>
+      <div className="round-counter">
+        {" "}
+        Global Score: {globalScore} / {round * 3}
+      </div>
 
-    {/* if round == 0 ? welcome screen + btn : game page at round 1+ */}
+      {/* if round == 0 ? welcome screen + btn : game page at round 1+ */}
 
       {questions}
-      {gameEnded ? (<button className="new-game"
-                onClick={() => newGame()}
-                >Play Again?</button>)
-
-            :  (<button className="submit"
-                onClick={()=> setGameEnded(true)}
-                >Submit</button>) }
+      {gameEnded ? (
+        <button className="new-game" onClick={() => newGame()}>
+          Play Again?
+        </button>
+      ) : (
+        <button className="submit" onClick={() => setGameEnded(true)}>
+          Submit
+        </button>
+      )}
 
       {gameEnded && <div> correct = {result}/NUM OF QUESTIONS</div>}
     </div>
---
+++ quizzical/src/fakeAPI.js
@@ -0,0 +1,49 @@
+export default {
+  "response_code": 0,
+  "results": [
+    {
+      "category": "Science & Nature",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "Who is the chemical element Curium named after?",
+      "correct_answer": "Marie &amp; Pierre Curie",
+      "incorrect_answers": [
+        "The Curiosity Rover",
+        "Curious George",
+        "Stephen Curry"
+      ]
+    },
+    {
+      "category": "Entertainment: Video Games",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "Who is the founder of Team Fortress 2&#039;s fictional company &quot;Mann Co&quot;?",
+      "correct_answer": "Zepheniah Mann",
+      "incorrect_answers": ["Cave Johnson", "Wallace Breem", "Saxton Hale"]
+    },
+    {
+      "category": "Science: Mathematics",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "What Greek letter is used to signify summation?",
+      "correct_answer": "Sigma",
+      "incorrect_answers": ["Delta", "Alpha", "Omega"]
+    },
+    {
+      "category": "Sports",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "Which team won 2014 FIFA World Cup in Brazil?",
+      "correct_answer": "Germany",
+      "incorrect_answers": ["Argentina", "Brazil", "Netherlands"]
+    },
+    {
+      "category": "Entertainment: Video Games",
+      "type": "multiple",
+      "difficulty": "hard",
+      "question": "In &quot;Team Fortress 2&quot;, what is the fastest taunt kill that can be pulled off?",
+      "correct_answer": "Showdown",
+      "incorrect_answers": ["Hadouken", "Organ Grinder", "Skewer"]
+    }
+  ]
+}
